bd985de45fa09f3766225e5fb651df4e3c10d6eb
Jahongir <alimovj727@gmail.com>
2025-03-18 13:38:36 +0500

    Added API documentation using Laravel Swagger

--- database/migrations/2025_03_18_083618_create_tasks_table.php
+++ database/migrations/2025_03_18_083618_create_tasks_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
-            
+            $table->text('description')->nullable();
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
